0d14b422fa8e6a90d00131cd0b280eab79951092
AdonNeet <baradontap@gmail.com>
2025-05-20 09:51:32 +0700

    chore: middleware and jwt

---
+++ src/lib/middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+const { verifyToken } = require('../services/jwtService');
+
+const authMiddleware = async (request, h) => {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return h.response({ error: 'Unauthorized: Token missing' }).code(401).takeover();
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = verifyToken(token);
+    request.auth = { credentials: decoded };
+    return h.continue;
+  } catch (err) {
+    if (err.name === 'TokenExpiredError') {
+      return h.response({ error: 'Expired: Token expired' }).code(401).takeover();
+    }
+    return h.response({ error: 'Unauthorized: Invalid token' }).code(401).takeover();
+  }
+};
+
+module.exports = authMiddleware;
