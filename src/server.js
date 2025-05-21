ce7b85c41a2b84be3692a10b2757d3c3b50ded62
AdonNeet <baradontap@gmail.com>
2025-05-21 20:28:32 +0700

    chore: antoher fix the bug on CORS ISSSUE

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const init = async () => {
       return h
         .response("OK")
         .code(200)
-        .header("Access-Control-Allow-Origin", "")
+        .header("Access-Control-Allow-Origin", "*")
         .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
         .header("Access-Control-Allow-Headers", "Content-Type, x-api-key");
     },
