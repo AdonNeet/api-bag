b48054e6123323072bcd226675212615c52a732a
AdonNeet <baradontap@gmail.com>
2025-05-22 14:55:38 +0700

    chore: I forgot to change the validations

--- src/validations/orderSchema.js
+++ src/validations/orderSchema.js
@@ -5,6 +5,7 @@ const orderSchemas = {
   addOrder: Joi.object({
     order_name: Joi.string().required(),
     typeorder: Joi.string().required(),
+    quantity: Joi.string().required(),
     note: Joi.string().allow('').optional(),
     start_date: Joi.date().required(),
     due_date: Joi.date().required()
@@ -13,6 +14,7 @@ const orderSchemas = {
   updateOrder: Joi.object({
     order_name: Joi.string().required(),
     typeorder: Joi.string().required(),
+    quantity: Joi.string().optional(),
     note: Joi.string().allow('').optional(),
     statusOrder: Joi.string().required(),
     start_date: Joi.date().required(),
