2f134bed837c1a6abf0df0e14aad5e6afe743a98
AdonNeet <baradontap@gmail.com>
2025-05-22 09:59:28 +0700

    chore: redisClient again

--- src/config/redisClient.js
+++ src/config/redisClient.js
@@ -6,7 +6,7 @@ const client = createClient({
   password: process.env.REDIS_PASS,
   socket: {
     host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
+    port: Number(process.env.REDIS_PORT),
   },
 });
 
